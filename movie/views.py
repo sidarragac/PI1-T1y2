b62070ef3cf78eefae134f01d2522211c9376958
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-08-01 10:20:06 -0500

    Cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     # return HttpResponse("<h1>Welcome to the home page</h1>")
-    return render(request, "home.html", {'name': 'Santiago Idarraga'})
+    return render(request, "home.html", {'name': 'Santiago Idarraga Ceballos'})
 
 def about(request):
     return HttpResponse("<h1>Welcome to the home about</h1>")
