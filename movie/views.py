32d668c3b7640255e47bace48995874672b656b6
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-08-01 12:00:45 -0500

    Added condition to filter  movies

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Movie
 # Create your views here.
 
 def home(request):
     # return HttpResponse("<h1>Welcome to the home page</h1>")
-    return render(request, "home.html", {'name': 'Santiago Idarraga Ceballos'})
+    # return render(request, "home.html", {'name': 'Santiago Idarraga Ceballos'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     # return HttpResponse("<h1>Welcome to the home about</h1>")
