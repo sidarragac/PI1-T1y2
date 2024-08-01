cfd96f66921403c51e8d22f9588a8158da2681a2
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-08-01 10:16:47 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    # return HttpResponse("<h1>Welcome to the home page</h1>")
+    return render(request, "home.html", {'name': 'Santiago Idarraga'})
+
+def about(request):
+    return HttpResponse("<h1>Welcome to the home about</h1>")
