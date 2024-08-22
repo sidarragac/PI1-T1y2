85d3b21e86a559129094bb87d985f88107181cd7
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-08-22 11:52:43 -0500

    Added shared template file path

--- moviesreviews/settings.py
+++ moviesreviews/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'moviesreviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviesreviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
