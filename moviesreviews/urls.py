74eec9b797616342fa4a92b4e635acaf4b15738a
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-08-22 09:56:28 -0500

    Created news app and updated required info

--- moviesreviews/urls.py
+++ moviesreviews/urls.py
@@ -24,7 +24,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
-    path('about/', movieViews.about)
+    path('about/', movieViews.about),
+    path('news/', include('news.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
