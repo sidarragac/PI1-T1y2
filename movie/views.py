4f707ddbe7acf6bed280a82622dbe9d0e1cd334d
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-08-23 23:54:13 -0500

    Writted code to build graphics

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 # Create your views here.
 
 def home(request):
@@ -16,3 +20,44 @@ def home(request):
 def about(request):
     # return HttpResponse("<h1>Welcome to the home about</h1>")
     return render(request, "about.html")
+
+def statisticsView(request):
+    matplotlib.use("Agg")
+    allMovies = Movie.objects.all()
+    movieCountsByYear = {}
+    
+    for movie in allMovies:
+        year = movie.year if movie.year else 'None'
+        if year in movie:
+            movieCountsByYear[year]+=1
+        else:
+            movieCountsByYear[year] = 1 
+        
+    barWidth = 0.5
+    barPositions = range(len(movieCountsByYear))
+    
+    #Se crea gráfico de barras
+    plt.bar(barPositions, movieCountsByYear.values(), width=barWidth, align='center')
+    
+    #Personalización del gráfico
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(barPositions, movieCountsByYear.keys(), rotation=90)
+    
+    #Espaciado entre barras
+    plt.subplots_adjust(bottom=0.3)
+    
+    #Se guarda la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    
+    #Convertir imagen en base64
+    imagePNG = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(imagePNG)
+    graphic = graphic.decode('utf-8')
+    
+    return render(request, 'statistics.html', {'graphic': graphic})
