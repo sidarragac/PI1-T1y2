857681aba222f8b50d40694d81faa59600eccedc
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-08-24 01:14:11 -0500

    Corrected static files dirs

--- moviesreviews/settings.py
+++ moviesreviews/settings.py
@@ -129,6 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    'moviesreviews/static/',
+    os.path.join(BASE_DIR, "moviesreviews/static/"),
 ]
